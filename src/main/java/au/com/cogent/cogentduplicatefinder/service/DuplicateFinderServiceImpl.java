404a06105eeb20d4e564aa7555e05b7a4e9ecabc
Amila Sembunaidelage <amila.sembunaidelage@wellcomww.com.au>
2020-07-25 11:27:59 +1000

    Add the 'DuplicateFinderServiceImpl' class
    - Added the 'DuplicateFinderServiceImpl' class by implementing the 'IDuplicateFinderService' interface

---
+++ src/main/java/au/com/cogent/cogentduplicatefinder/service/DuplicateFinderServiceImpl.java
@@ -0,0 +1,14 @@
+package au.com.cogent.cogentduplicatefinder.service;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicateFinderServiceImpl implements IDuplicateFinderService {
+
+	public List<String> getFilePathsWithDuplicates(Map<String, List<String>> fileList, File directory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
