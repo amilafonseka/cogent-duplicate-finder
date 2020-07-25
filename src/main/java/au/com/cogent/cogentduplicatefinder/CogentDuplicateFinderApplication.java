908b3a6f6e733bb566b8a07c85e2f35882a56d1b
Amila Sembunaidelage <amila.sembunaidelage@wellcomww.com.au>
2020-07-25 11:15:07 +1000

    Entry point to the application
    Added the entry point to the application, CogentDuplicationFinderApplication class
    which includes the main method.

---
+++ src/main/java/au/com/cogent/cogentduplicatefinder/CogentDuplicateFinderApplication.java
@@ -0,0 +1,17 @@
+package au.com.cogent.cogentduplicatefinder;
+
+/**
+ * Entry point to the application
+ * 
+ * @author amilasembunaidelage
+ *
+ */
+public class CogentDuplicateFinderApplication {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
