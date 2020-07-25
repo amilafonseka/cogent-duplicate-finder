3e988b1c49b82fbd0ad92a1eb09c23336aa11647
Amila Sembunaidelage <amila.sembunaidelage@wellcomww.com.au>
2020-07-25 17:21:27 +1000

    The main method has been implemented
    - The main method has been implemented to pass the user entered directory
    to the DuplicateFinderService and display the duplicate file paths

--- src/main/java/au/com/cogent/cogentduplicatefinder/CogentDuplicateFinderApplication.java
+++ src/main/java/au/com/cogent/cogentduplicatefinder/CogentDuplicateFinderApplication.java
@@ -1,5 +1,12 @@
 package au.com.cogent.cogentduplicatefinder;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import au.com.cogent.cogentduplicatefinder.service.DuplicateFinderServiceImpl;
+
 /**
  * Entry point to the application
  * 
@@ -10,7 +17,44 @@ public class CogentDuplicateFinderApplication {
 
 	public static void main(String[] args) {
 		
+		DuplicateFinderServiceImpl duplicateFinder = 
+				new DuplicateFinderServiceImpl();
 		
+		if (args.length < 1) {
+			
+			System.out.println("Please enter a directory path");
+			return;
+			
+		}
+		
+		File directory = new File(args[0]);
+		
+		if(!directory.isDirectory()) {
+			
+			System.out.println("Entered directory does not exist");
+			return;
+			
+		}
+		
+		Map<String, List<String>> lists = new HashMap<String, List<String>>();
+		
+		duplicateFinder.getFilePathsWithDuplicates(lists, directory);
+		
+		for ( List<String> list : lists.values()) {
+			
+			if ( list.size() > 1 ) {
+				
+				System.out.println("\n");
+				for(String file : list) {
+					
+					System.out.println(file);
+					
+				}
+				
+			}
+		}
+		
+		System.out.println("\n");
 
 	}
 
