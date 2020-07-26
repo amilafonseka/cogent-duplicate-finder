6da4ec188b209a5937df32d0d5e66062278730df
Amila Sembunaidelage <amila.sembunaidelage@wellcomww.com.au>
2020-07-26 16:22:56 +1000

    Add DuplicateFinderInputValidationService to validate the user input

---
+++ src/main/java/au/com/cogent/cogentduplicatefinder/service/DuplicateFinderInputValidationService.java
@@ -0,0 +1,36 @@
+package au.com.cogent.cogentduplicatefinder.service;
+
+import au.com.cogent.cogentduplicatefinder.exception.ArgumentFormatException;
+
+/**
+ * Validates arguments input
+ * 
+ * @author amilasembunaidelage
+ *
+ */
+public class DuplicateFinderInputValidationService implements IDuplicateFinderInputValidationService {
+
+	/**
+	 * Validates the arguments input
+	 *
+	 * @param argument input
+	 * @throws ArgumentFormatException
+	 */
+	@Override
+	public void validateDuplicateFinderArgumentInput(String argument) throws ArgumentFormatException {
+		
+		checkNullAndEmpty(argument);
+
+	}
+	
+	private void checkNullAndEmpty(String argument) 
+			throws ArgumentFormatException {
+		if(argument == null) {
+			throw new ArgumentFormatException("Transaction text is NULL");
+		
+		} else if(argument.isEmpty()) {
+			throw new ArgumentFormatException("Transaction text is empty");
+		}
+	}
+
+}
